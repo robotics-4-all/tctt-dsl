79042a0993b7c2cdce1b9434eb786ca3bb097e08
Manos Tsardoulias <etsardou@gmail.com>
2024-11-05 15:59:27 +0200

    Fixed syntax a bit, added visuals syntax

--- tctt/grammar/check_syntax.py
+++ tctt/grammar/check_syntax.py
@@ -5,7 +5,7 @@ meta_model = metamodel_from_file('syntax/tctt.tx')
 
 # Parse the file to be validated
 try:
-    model = meta_model.model_from_file('examples/test.txt')
+    model = meta_model.model_from_file('examples/test.yaml')
     print("Syntax is valid!")
 except Exception as e:
     print(f"Syntax Error: {e}")
